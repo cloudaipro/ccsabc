de4fd8889ac400358c3cb24864d4a8d804bd3a1d
DellMac <admin@DellMacs-MacBook-Pro.local>
2019-07-27 17:24:43 -0700

    .

--- index.js
+++ index.js
@@ -106,5 +106,5 @@ app.post('/chargeForCookie', async (request, response) => {
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('=== Your app is listening on port ' + listener.address().port);
 });
